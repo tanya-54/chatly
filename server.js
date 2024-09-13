ac756965ac29a03a6ab5396e59a7152d3cb382b3
Tanya Dubey <dubeytanya2312@gmail.com>
2024-09-13 22:03:01 +0530

    Add .gitignore and remove node_modules from tracking

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express') ;
+const app = express()
+const http= require('http').createServer(app);
+
+const PORT = process.env.PORT || 3000
+
+http.listen( PORT , ()=>{
+    console.log(`listening on ${PORT}`);
+})
+
+app.get('/' , (req, res) =>{
+    res.sendFile(__dirname + '/index.html');
+})
